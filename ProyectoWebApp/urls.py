5d24afb97eb89a95b3b90f7f9ccc897d1b1b1776
sturniolo <br1.sturniolo@gmail.com>
2023-12-01 14:03:20 -0300

    [ADD & FIX] gestion de imagenes en el panel admin

--- ProyectoWebApp/urls.py
+++ ProyectoWebApp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from ProyectoWebApp import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
 
@@ -10,3 +12,5 @@ urlpatterns = [
     path('contacto/', views.contacto, name="Contacto"),
 
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
