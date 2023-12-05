b5ebb8918d90e4a3f5e5fe551b27d16f1ea63225
sturniolo <br1.sturniolo@gmail.com>
2023-12-05 11:12:37 -0300

    [ADD & IMPORTANT]funcionalidad al carro, y desactivacion del carro

--- autenticacion/views.py
+++ autenticacion/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
+from django.contrib.auth import login
 
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 # Create your views here.
 
@@ -13,4 +15,18 @@ class VRegistro(View):
 
 
     def post(self, request):
-        pass
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+
+            usuario = form.save()
+
+            login(request, usuario)
+
+            return redirect('Home')
+    
+        else:
+            for msg in form.error_messages:
+                messages.error(request, form.error_messages[msg])
+            
+            return render(request, "registro/registro.html", {"form":form})
